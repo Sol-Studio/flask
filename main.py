931fd69e6dd0c1dedeacdd8ce71aa2d575ed8ac7
sol-studio <cloud8862@gmail.com>
2021-01-25 15:34:18 +0900

    pretty code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, send_file, jsonify, url_for
+from flask import Flask, render_template, request, redirect, send_file, jsonify
 from werkzeug.utils import secure_filename
 import os
 import random
@@ -8,13 +8,11 @@ import logging
 import json
 
 
-
 app = Flask(__name__)
 
 
 logging.basicConfig(filename=time.strftime('%Y-%m-%d', time.localtime(time.time())) + "_log.log",
                     level=logging.DEBUG)
-
 
 
 string_pool = list("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -28,9 +26,9 @@ def get_id(file):
         _id = random.choice(string_pool) + random.choice(string_pool)
 
         cur.execute('SELECT * FROM file WHERE (file=?)', (file,))
-        if cur.fetchall() == []:
+        if not cur.fetchall():
             cur.execute('SELECT * FROM file WHERE (id=?)', (_id, ))
-            if cur.fetchall() == []:
+            if not cur.fetchall():
                 conn.commit()
                 conn.close()
                 return _id
@@ -55,10 +53,8 @@ def read_data(_id):
     try:
         return _return[0][1]
 
-
-    except Exception as e:
-        del e
-        return None
+    except KeyError:
+        return True
 
 
 @app.route("/", methods=['GET'])
@@ -84,7 +80,7 @@ def upload():
 
 @app.route("/complete", methods=['GET', 'POST'])
 def file_upload():
-    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    # ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
@@ -108,9 +104,9 @@ def download():
 @app.route("/d/<file_id>")
 def get_profile(file_id):
     file = read_data(file_id)
-    if file == None:
+    if file:
         file = "잘못된 아이디입니다."
-    return render_template("do.html", file=file)
+    return render_template("file/do.html", file=file)
 
 
 @app.route("/do/<file>")
@@ -128,21 +124,23 @@ def gogo():
 # ai
 @app.route("/ai")
 def ai():
-    return redirect("https://web-chat.global.assistant.watson.cloud.ibm.com/preview.html?region=kr-seo&integrationID=73dbc727-82aa-4186-bb30-8f3acd19fb76&serviceInstanceID=4d5d9ef3-d64c-48e9-bd97-f14756c46928")
+    return redirect("https://web-chat.global.assistant.watson.cloud.ibm.com/"
+                    + "preview.html?region=kr-seo&integrationID=73dbc727-82aa-41"
+                    + "86-bb30-8f3acd19fb76&serviceInstanceID=4d5d9ef3-d64c-48e9-bd97-f14756c46928")
 
 
 @app.route("/bot", methods=['POST', 'GET'])
 def bot():
     params = json.loads(request.get_data(), encoding='utf-8')
     print(params)
-    return jsonify({"doing":"asdf"})
+    return jsonify({"doing": "asdf"})
 
 
 # 404
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(e):
+    type(e)
     return redirect("/?err=404")
-
 
 
 app.run(host="0.0.0.0", port=4999, debug=True)
